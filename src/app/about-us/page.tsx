4e44ae6c4f0ad55ca13d7419730a299dd55cd5ef
Doan <doan.tt.269@gmail.com>
2025-06-03 19:38:40 -0700

    compelted homepage according to design

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -6,13 +6,11 @@ import "../page.css";
 export default function Page() {
   return (
     <div className="relative">
-      <Header />
       <div className="shineBg_body_top_left"></div>
       <div className="green-shine-middle-right"></div>
       <div className="bg-transparent rounded-[20px] border border-white/10 backdrop-blur-lg p-8 md:mx-[56px] mt-[16px]">
         <h1 className="text-white">Hello this is About US Page</h1>
       </div>
-      <FireSightFooter />
     </div>
   );
 }
