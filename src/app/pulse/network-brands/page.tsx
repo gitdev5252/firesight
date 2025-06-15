72f629fbc25b45b9f23e5badac5487c7f5c29c80
Doan <doan.tt.269@gmail.com>
2025-06-14 17:31:42 -0700

    hello@firesight.ai

--- src/app/pulse/network-brands/page.tsx
+++ src/app/pulse/network-brands/page.tsx
@@ -9,7 +9,6 @@ export default function Page() {
   return (
     <>
       <div className="w-full relative">
-
         <div className="blue-shine-pulse-overview w-[41.8vw] h-[41.8vw] top-[-6.46vw] right-[-9.514vw] z-[-2000]"></div>
         <div className="bg-[url('/images/color-pattern-network-brands.svg')] bg-cover w-full h-[1970px] top-[0px] absolute z-[-10000] opacity-70 md:block hidden"></div>
       </div>
@@ -36,10 +35,9 @@ export default function Page() {
         </h3>
         <Button
           variant="outline"
-
           className="cursor-pointer gradient-border-btn text-[16px] md:mt-[35px] mt-[24px] bg-transparent rounded-full md:px-[50px] px-[36px] md:py-[20px] py-[12px] text-white hover:text-white"
         >
-          Contact Us
+          <a href="mailto:hello@firesight.ai">Contact Us</a>
         </Button>
       </div>
       <div className="flex flex-wrap justify-around md:mx-[58px] mx-[16px] text-white md:gap-[30px] gap-[24px] pb-[30px] relative">
