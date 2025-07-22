132dae97104d5e3379155ff2eea93c85ea7f541e
Tuan Trung Doan <doan.tt.269@gmail.com>
2025-07-23 04:44:18 +0700

    deleted unused variables.

--- src/app/ai-impact/home/category/[occupation]/page.tsx
+++ src/app/ai-impact/home/category/[occupation]/page.tsx
@@ -67,13 +67,14 @@ export default function OccupationPage() {
   useEffect(() => {
     setTabIndex(3);
   }, [occupation]);
-  const filteredCategories = searchTerm.trim() === ""
-    ? occupations
-    : occupations.filter((cat) =>
-      cat.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-  const renderedListLength =
-    tabIndex === 3 ? filteredCategories.length : sortedOccupations.length;
+  const filteredCategories =
+    searchTerm.trim() === ""
+      ? occupations
+      : occupations.filter((cat) =>
+          cat.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+  // const renderedListLength =
+  //   tabIndex === 3 ? filteredCategories.length : sortedOccupations.length;
 
   return (
     <div>
@@ -118,9 +119,7 @@ export default function OccupationPage() {
           // className={`flex flex-col sm:flex-row flex-wrap ${renderedListLength !== 2 ? "md:justify-between" : ""
           //   } lg:gap-y-9 gap-y-5 gap-x-5 text-white font-bold lg:text-2xl text-[16px] leading-normal h-[800px] overflow-y-auto p-[40px] mb-[40px]`}
           className={`flex flex-col sm:flex-row flex-wrap justify-start gap-x-5 gap-y-7 md:gap-y-9 text-white font-bold lg:text-2xl text-[16px] leading-normal h-[800px] overflow-y-auto p-[40px] mb-[40px]`}
-
         >
-
           {tabIndex === 3 ? (
             // Render categories as links, filtered by searchTerm
             (() => {
