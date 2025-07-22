d7fe19d80972954d4dcd3459525fdb64dbf0a81a
Tuan Trung Doan <doan.tt.269@gmail.com>
2025-07-23 04:31:45 +0700

    something changed.....

--- src/app/ai-impact/home/page.tsx
+++ src/app/ai-impact/home/page.tsx
@@ -92,7 +92,7 @@ export default function Page() {
 
   return (
     <>
-      <div className="w-full lg:my-29 md:mt-9 mt-11 md:mb-13 mb-5 mb-7">
+      <div className="w-full lg:my-29 md:mt-9 mt-11 md:mb-13 mb-7">
         <TabBar
           type={1}
           selectedIndex={sortIndex}
