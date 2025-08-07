9e1006f4d83241401ff167d377c93937a1e512a0
Tuan Trung Doan <doan.tt.269@gmail.com>
2025-08-08 06:02:33 +0700

    removed unused variables

--- src/app/session/page.tsx
+++ src/app/session/page.tsx
@@ -1,9 +1,3 @@
-import FireSightFooter from "@/layouts/FireSightFooter";
 export default function SessionPage() {
-  return (
-    <div className="w-full flex">
-      Hello this is session page
-      {/* <FireSightFooter children={undefined} /> */}
-    </div>
-  );
+  return <div className="w-full flex">Hello this is session page</div>;
 }
