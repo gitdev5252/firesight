c5f6504bb85d19e7d6f0bea1b5bcb4727fa9bdd3
Tuan Trung Doan <doan.tt.269@gmail.com>
2025-08-18 19:36:14 +0700

    refactor: Simplify FireSightFooter rendering logic in RootLayout - Updated the conditional rendering of FireSightFooter to separate pathname checks for improved clarity and maintainability.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,15 +25,22 @@ export default function RootLayout({
     <html lang="en">
       <body className={clsx(lekton.className, "bg-[#080B16] relative m-0")}>
         <ClientLayout>{children}</ClientLayout>
-        {pathname === "/" ||
-          (pathname === "/pulse/overview" && (
-            <FireSightFooter>
-              <div className="green-shine-footer-mobile md:hidden block z-[-2342]"></div>
-              <div className="blue-shine-pulse-overview w-[min(602px,41.8vw)] h-[min(602px,41.8vw)] bottom-[7.7vw] right-[19.3vw] md:block hidden"></div>
-              <div className="blue-shine-pulse-overview bottom-[-25.555vw] right-[-20.277vw] w-[min(602px,41.8vw)] h-[min(602px,41.8vw)] md:block hidden"></div>
-              <div className="blue-shine-pulse-overview bottom-[-32.847vw] left-[-16.666vw] w-[min(602px,41.8vw)] h-[min(602px,41.8vw)] md:block hidden"></div>
-            </FireSightFooter>
-          ))}
+        {pathname === "/" && (
+          <FireSightFooter>
+            <div className="green-shine-footer-mobile md:hidden block z-[-2342]"></div>
+            <div className="blue-shine-pulse-overview w-[min(602px,41.8vw)] h-[min(602px,41.8vw)] bottom-[7.7vw] right-[19.3vw] md:block hidden"></div>
+            <div className="blue-shine-pulse-overview bottom-[-25.555vw] right-[-20.277vw] w-[min(602px,41.8vw)] h-[min(602px,41.8vw)] md:block hidden"></div>
+            <div className="blue-shine-pulse-overview bottom-[-32.847vw] left-[-16.666vw] w-[min(602px,41.8vw)] h-[min(602px,41.8vw)] md:block hidden"></div>
+          </FireSightFooter>
+        )}
+        {pathname === "/pulse/overview" && (
+          <FireSightFooter>
+            <div className="green-shine-footer-mobile md:hidden block z-[-2342]"></div>
+            <div className="blue-shine-pulse-overview w-[min(602px,41.8vw)] h-[min(602px,41.8vw)] bottom-[7.7vw] right-[19.3vw] md:block hidden"></div>
+            <div className="blue-shine-pulse-overview bottom-[-25.555vw] right-[-20.277vw] w-[min(602px,41.8vw)] h-[min(602px,41.8vw)] md:block hidden"></div>
+            <div className="blue-shine-pulse-overview bottom-[-32.847vw] left-[-16.666vw] w-[min(602px,41.8vw)] h-[min(602px,41.8vw)] md:block hidden"></div>
+          </FireSightFooter>
+        )}
       </body>
     </html>
   );
