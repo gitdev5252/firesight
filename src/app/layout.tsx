c74dac162f0a9415ac9adad27c955e5c30a20211
Tuan Trung Doan <doan.tt.269@gmail.com>
2025-08-19 09:23:16 +0700

    completed session-start page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,20 +26,24 @@ export default function RootLayout({
       <body className={clsx(lekton.className, "bg-[#080B16] relative m-0")}>
         <ClientLayout>{children}</ClientLayout>
         {pathname === "/" && (
-          <FireSightFooter>
-            <div className="green-shine-footer-mobile md:hidden block z-[-2342]"></div>
-            <div className="blue-shine-pulse-overview w-[min(602px,41.8vw)] h-[min(602px,41.8vw)] bottom-[7.7vw] right-[19.3vw] md:block hidden"></div>
-            <div className="blue-shine-pulse-overview bottom-[-25.555vw] right-[-20.277vw] w-[min(602px,41.8vw)] h-[min(602px,41.8vw)] md:block hidden"></div>
-            <div className="blue-shine-pulse-overview bottom-[-32.847vw] left-[-16.666vw] w-[min(602px,41.8vw)] h-[min(602px,41.8vw)] md:block hidden"></div>
-          </FireSightFooter>
+          <div className="w-full px-4 md:px-14">
+            <FireSightFooter>
+              <div className="green-shine-footer-mobile md:hidden block z-[-2342]"></div>
+              <div className="blue-shine-pulse-overview w-[min(602px,41.8vw)] h-[min(602px,41.8vw)] bottom-[7.7vw] right-[19.3vw] md:block hidden"></div>
+              <div className="blue-shine-pulse-overview bottom-[-25.555vw] right-[-20.277vw] w-[min(602px,41.8vw)] h-[min(602px,41.8vw)] md:block hidden"></div>
+              <div className="blue-shine-pulse-overview bottom-[-32.847vw] left-[-16.666vw] w-[min(602px,41.8vw)] h-[min(602px,41.8vw)] md:block hidden"></div>
+            </FireSightFooter>
+          </div>
         )}
         {pathname === "/pulse/overview" && (
-          <FireSightFooter>
-            <div className="green-shine-footer-mobile md:hidden block z-[-2342]"></div>
-            <div className="blue-shine-pulse-overview w-[min(602px,41.8vw)] h-[min(602px,41.8vw)] bottom-[7.7vw] right-[19.3vw] md:block hidden"></div>
-            <div className="blue-shine-pulse-overview bottom-[-25.555vw] right-[-20.277vw] w-[min(602px,41.8vw)] h-[min(602px,41.8vw)] md:block hidden"></div>
-            <div className="blue-shine-pulse-overview bottom-[-32.847vw] left-[-16.666vw] w-[min(602px,41.8vw)] h-[min(602px,41.8vw)] md:block hidden"></div>
-          </FireSightFooter>
+          <div className="w-full px-4 md:px-14">
+            <FireSightFooter>
+              <div className="green-shine-footer-mobile md:hidden block z-[-2342]"></div>
+              <div className="blue-shine-pulse-overview w-[min(602px,41.8vw)] h-[min(602px,41.8vw)] bottom-[7.7vw] right-[19.3vw] md:block hidden"></div>
+              <div className="blue-shine-pulse-overview bottom-[-25.555vw] right-[-20.277vw] w-[min(602px,41.8vw)] h-[min(602px,41.8vw)] md:block hidden"></div>
+              <div className="blue-shine-pulse-overview bottom-[-32.847vw] left-[-16.666vw] w-[min(602px,41.8vw)] h-[min(602px,41.8vw)] md:block hidden"></div>
+            </FireSightFooter>
+          </div>
         )}
       </body>
     </html>
