fa1e4755c3e313ec92a876aa7b8f80a5b29cafd2
Tuan Trung Doan <doan.tt.269@gmail.com>
2025-08-21 09:25:44 +0700

    refactor: Simplify footer rendering logic and enhance accessibility in Home component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,30 +21,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   const pathname = usePathname();
+  const renderFooter = () => (
+    <div className="w-full">
+      <FireSightFooter>
+        <div className="green-shine-footer-mobile md:hidden block z-[-2342]"></div>
+        <div className="blue-shine-pulse-overview w-[min(602px,41.8vw)] h-[min(602px,41.8vw)] bottom-[7.7vw] right-[19.3vw] md:block hidden"></div>
+        <div className="blue-shine-pulse-overview bottom-[-25.555vw] right-[-20.277vw] w-[min(602px,41.8vw)] h-[min(602px,41.8vw)] md:block hidden"></div>
+        <div className="blue-shine-pulse-overview bottom-[-32.847vw] left-[-16.666vw] w-[min(602px,41.8vw)] h-[min(602px,41.8vw)] md:block hidden"></div>
+      </FireSightFooter>
+    </div>
+  );
   return (
     <html lang="en">
       <body className={clsx(lekton.className, "bg-[#080B16] relative m-0")}>
         <ClientLayout>{children}</ClientLayout>
-        {pathname === "/" && (
-          <div className="w-full">
-            <FireSightFooter>
-              <div className="green-shine-footer-mobile md:hidden block z-[-2342]"></div>
-              <div className="blue-shine-pulse-overview w-[min(602px,41.8vw)] h-[min(602px,41.8vw)] bottom-[7.7vw] right-[19.3vw] md:block hidden"></div>
-              <div className="blue-shine-pulse-overview bottom-[-25.555vw] right-[-20.277vw] w-[min(602px,41.8vw)] h-[min(602px,41.8vw)] md:block hidden"></div>
-              <div className="blue-shine-pulse-overview bottom-[-32.847vw] left-[-16.666vw] w-[min(602px,41.8vw)] h-[min(602px,41.8vw)] md:block hidden"></div>
-            </FireSightFooter>
-          </div>
-        )}
-        {pathname === "/pulse/overview" && (
-          <div className="w-full">
-            <FireSightFooter>
-              <div className="green-shine-footer-mobile md:hidden block z-[-2342]"></div>
-              <div className="blue-shine-pulse-overview w-[min(602px,41.8vw)] h-[min(602px,41.8vw)] bottom-[7.7vw] right-[19.3vw] md:block hidden"></div>
-              <div className="blue-shine-pulse-overview bottom-[-25.555vw] right-[-20.277vw] w-[min(602px,41.8vw)] h-[min(602px,41.8vw)] md:block hidden"></div>
-              <div className="blue-shine-pulse-overview bottom-[-32.847vw] left-[-16.666vw] w-[min(602px,41.8vw)] h-[min(602px,41.8vw)] md:block hidden"></div>
-            </FireSightFooter>
-          </div>
-        )}
+        {["/", "/pulse/overview"].includes(pathname) && renderFooter()}
       </body>
     </html>
   );
